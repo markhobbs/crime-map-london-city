2d52cafbf3c762018b28fdeefb5749ea6399e6fd
Mark Hobbs <hobbs1m@hotmail.com>
2017-08-23 22:46:46 +0100

    Some additional styling

--- src/components/GoogleMapCrime.js
+++ src/components/GoogleMapCrime.js
@@ -1,39 +1,87 @@
-import React, { Component } from 'react';
+import fetch from "isomorphic-fetch";
+
+import {
+  default as React,
+  Component,
+} from "react";
+
+import { withGoogleMap, GoogleMap, Marker} from 'react-google-maps/lib';
+import MarkerClusterer from 'react-google-maps/lib/addons/MarkerClusterer';
+
 import logo from './../logo.svg';
 import './../App.css';
 
-import { withGoogleMap, GoogleMap} from 'react-google-maps/lib';
 
-/*
-* Sample From: https://developers.google.com/maps/documentation/javascript/examples/map-simple
-*/
-const SimpleMapExampleGoogleMap = withGoogleMap(props => (
+const MarkerClustererExampleGoogleMap = withGoogleMap(props => (
+
     <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-    />
+        defaultZoom={13}
+        defaultCenter={{ lat: 51.509865, lng: -0.118092 }}>
+        <MarkerClusterer
+            averageCenter
+            enableRetinaIcons
+            gridSize={60}
+        >
+            {props.markers.map(marker => (
+                <Marker
+                    //position={{ lat: parseInt(marker.Latitude,10)+1, lng: parseInt(marker.Longitude,10)+1}}
+                    //position={{ lat: 51.509865, lng: -0.118092 }}   
+                    position={{ lat: parseInt(marker.location.latitude,10), lng: parseInt(marker.location.longitude,10)}}                    
+                    key={marker.id}
+                />
+            ))}
+        </MarkerClusterer>
+    </GoogleMap>
 ));
 
+
+
 class GoogleMapCrime extends Component {
+
+    state = {
+        markers: [],        
+    }    
+    
+    /*componentDidMount() {
+        fetch('api.json')
+        .then(res => res.json())
+        .then(data => {
+            console.log("data: ", data)
+            this.setState({ markers: data });
+        });
+    }*/
+
+    componentDidMount() {
+        fetch('https://data.police.uk/api/crimes-street/all-crime?poly=52.268,0.543:52.794,0.238:52.130,0.478&date=2017-01')
+        .then(res => res.json())
+        .then(data => {
+            //console.log(data)
+            this.setState({ markers: data });
+        });
+    }
+
+
     render() {
         return (
-          <div className="App" style={{width: 600, height: 800}}      >
-          <img src={logo} className="App-logo" alt="logo" />
-            <SimpleMapExampleGoogleMap
-            containerElement={
-                <div style={{ height: `100%` }} />
-            }
-            mapElement={
-                <div style={{ height: `100%` }} />
-            }
-            />            
-          </div>          
+            <div className="App">
+                <header>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>Crime Map UK</h1>                
+                    <nav/>
+                </header>
+                <content>                
+                    <MarkerClustererExampleGoogleMap
+                        containerElement={<div style={{ height: `100%` }} />}
+                        mapElement={<div style={{ height: `100%` }} />}
+                        markers={this.state.markers}
+                    />
+                </content>
+                <footer> 
+                    <img src={logo} className="App-logo" alt="logo" />Created with React
+                </footer>         
+            </div>
         );
       }
   }
   
-  export default GoogleMapCrime;
-
-
-
-  
+  export default GoogleMapCrime;  
